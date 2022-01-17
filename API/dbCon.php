b89fcc0d77b41a8ee58bdc4c475f993090f2c697
Sam Tes <bereket@Sams-MacBook.local>
2022-01-17 07:35:53 -0800

    modified dbCon file

--- API/dbCon.php
+++ API/dbCon.php
@@ -7,8 +7,8 @@ class ConnectDB {
   
   private $host = '127.0.0.1';
   private $user = 'root';
-  private $pass = 'bereket#3';
-  private $name = 'test';
+  private $pass = 'mypass';
+  private $name = 'dbName';
    
   // The db connection is established in the private constructor.
   private function __construct()
@@ -42,43 +42,4 @@ $options = [
 $instance = ConnectDB::getInstance();
 $con = $instance->getConnection();
 
-
-// $host = '127.0.0.1';
-// $db   = 'test';
-// $user = 'root';
-// $pass = 'bereket#3';
-// $charset = 'utf8mb4';
-
-// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-// $options = [
-//     PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
-//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-//     PDO::ATTR_EMULATE_PREPARES   => false,
-// ];
-// // var_dump(PDO::ATTR_DEFAULT_FETCH_MODE);
-// try {
-//      $con = new PDO($dsn, $user, $pass, $options);
-// } catch (\PDOException $e) {
-//      throw new \PDOException($e->getMessage(), (int)$e->getCode());
-// }
-
-//END CONNECTION 
-
-// $instance = ConnectDb::getInstance();
-// $con = $instance->getConnection();
-// // var_dump($con);
-
-// $stmt = $con->query('SELECT firstName FROM myUsers');
-// $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// echo json_encode($data);
-
-
-// $arr=[];
-// $i=0;
-// while ($row = $stmt->fetch())
-// {
-//     $arr[$i]= $row;
-//     $i++;
-// }
-// echo json_encode(['sucess'=>$arr]);
  ?>
